6917886ddc63a17086c06cb2aaca3f717188917e
Rafael Paes <rfaelpaes@gmail.com>
2020-06-18 18:25:53 -0300

    :rocket: componente padrão details

---
+++ src/components/Details/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {
+  Container, Text
+} from './styles'
+
+export default function Details() {
+  return (
+    <Container>
+      <Text>Apenas para criar um componente padrão</Text>
+    </Container>
+  )
+}
---
+++ src/components/Details/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Container = styled.View``
+
+const Text = styled.Text`
+  color: #FFF;
+  font-size: 16px;
+`
+
+export {
+  Container, Text
+}
