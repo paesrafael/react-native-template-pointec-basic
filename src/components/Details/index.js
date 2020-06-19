67baecc31d73372e6d5e115e9744aadd9e2ab697
Rafael Paes <rfaelpaes@gmail.com>
2020-06-19 12:54:49 -0300

    adicionando botão para o site

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { Linking } from 'react-native'
 
 import {
   Container, Text
 } from './styles'
 
 export default function Details() {
+  const OpenURLButton = ({ url, children }) => {
+    const handlePress = useCallback(async () => {
+      const supported = await Linking.canOpenURL(url)
+
+      if (supported)
+        await Linking.openURL(url)
+    }, [url])
+
+    return (
+      <Button
+        title={children}
+        onPress={handlePress}
+        underlayColor="#0050d4"
+      >
+        <Text
+          margin="15px 0"
+          fontSize="22px"
+        >Visite: www.pointec.dev</Text>
+      </Button>
+    )
+  }
+
   return (
     <Container>
-      <Text>Apenas para criar um componente padrão</Text>
+      <Text margin="5px 0 0 0">Apenas para criar um componente padrão</Text>
+
+      <OpenURLButton url="https://pointec.dev/" />
     </Container>
   )
 }
--- src/components/Details/styles.js
+++ src/components/Details/styles.js
@@ -3,10 +3,18 @@ import styled from 'styled-components'
 const Container = styled.View``
 
 const Text = styled.Text`
+  margin: ${props => props.margin ? props.margin : '0'};
   color: #FFF;
-  font-size: 16px;
+  font-size: ${props => props.fontSize ? props.fontSize : '16px'};
+  text-align: center;
+`
+
+const Button = styled.TouchableHighlight`
+  margin: 15px 0;
+  background-color: #0050d4;
+  border-radius: 10px;
 `
 
 export {
-  Container, Text
+  Container, Text, Button
 }
